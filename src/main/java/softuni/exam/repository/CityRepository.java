8e412cf6fa588b8bf9bb7603fa7f930514f0b836
Nikolay Bachvarov <nikolai_ba4@abv.bg>
2023-03-28 06:44:59 +0300

    Update CityRepository.java

--- src/main/java/softuni/exam/repository/CityRepository.java
+++ src/main/java/softuni/exam/repository/CityRepository.java
@@ -2,7 +2,7 @@ package softuni.exam.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import softuni
+import softuni.exam.models.entity.City;
 
 import java.util.Optional;
 
